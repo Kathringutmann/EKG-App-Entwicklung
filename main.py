1fb6e4b84a3411d2b312b1230517fb4e1c20f6fa
Kathringutmann <gk2575@mci4me.at>
2024-04-24 16:12:52 +0200

    Verschiebung der Bilder

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ names = get_name(person_data)
 #sessions State wird leer angelegt, solange 
 if 'current_user' not in st.session_state:
     st.session_state.current_user = None
-
+    
+#Überschrift
 st.title('EKG Analyse')
-# Eine Überschrift der zweiten Ebene
-st.write("## Versuchsperson auswählen")
 
-
-current_user = st.selectbox(
-    'Versuchsperson',
-    options = names, key="sbVersuchspersonen")
-
-st.write("currently selected user is: "+ current_user)
-
-# Paket zum anzeigen der Bilder
 from PIL import Image
-# Laden eines Bilds
-image = Image.open(find_person_data_by_name(current_user)["picture_path"])
-# Anzeigen eines Bilds mit Caption
-st.image(image, caption=st.session_state.current_user)
 
+col1, col2 = st.columns(2)
 
+with col1:
+   st.write("## Versuchsperson auswählen") #Unterüberschrift
+   current_user = st.selectbox(
+    'Versuchsperson',
+    options = names, key="sbVersuchspersonen")#Auswahlbox
+   image = Image.open(find_person_data_by_name(current_user)["picture_path"])
+
+   st.write("currently selected user is: "+ current_user)
+
+with col2:
+   st.header("Bild")
+   st.image(image, caption=st.session_state.current_user)
