eb1109ff23904ebcd4dfe87c394e2506dbe64ff1
gretaangebrand <ag9716@mci4me.at>
2024-06-04 10:09:29 +0200

    versuch find_peaks

--- ekgdata.py
+++ ekgdata.py
@@ -1,10 +1,15 @@
 import json
 import pandas as pd
 import plotly.express as px
+import scipy.signal as signal
 
 # %% Objekt-Welt
 
 # Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
+
+# Methode für find_peaks erstellen
+
+
 
 class EKGdata:
 
@@ -17,11 +22,19 @@ class EKGdata:
         self.data = ekg_dict["result_link"]
         self.df = pd.read_csv(self.data, sep='\t', header=None, names=['Messwerte in mV','Zeit in ms',])
 
+    def find_peaks(self):
+        x = self.df["Messwerte in mV"]
+        #thresold auf größeren Wert setzen, um nur Peaks zu finden, die höher als 360 mV sind
+        self.peaks = signal.find_peaks(x, height=None, threshold>=360, distance=None, prominence=None, width=None, wlen=None, rel_height=0.5, plateau_size=None)
+        return self.peaks
 
     def make_plot(self):
 
-        # Erstellte einen Line Plot, der ersten 2000 Werte mit der Zeit aus der x-Achse
-        self.fig = px.line(self.df.head(2000), x="Zeit in ms", y="Messwerte in mV")
+        # Erstellte einen Line Plot, mit der Zeit aus der x-Achse
+        self.fig = px.line(self.df, x="Zeit in ms", y="Messwerte in mV")
+
+        # show the peaks in the plot that are above the threshold
+        self.fig.add_scatter(x=self.df["Zeit in ms"].iloc[self.peaks[0]], y=self.df["Messwerte in mV"].iloc[self.peaks[0]], mode='markers', marker=dict(color='blue', size=8))
         #return self.fig 
 
 
@@ -33,3 +46,8 @@ if __name__ == "__main__":
     print(ekg_dict)
     ekg = EKGdata(ekg_dict)
     print(ekg.df.head())
+
+    ekg.find_peaks()
+    print(ekg.peaks)
+    ekg.make_plot()
+    ekg.fig.show()
