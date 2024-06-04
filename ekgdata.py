94dfd3f426422cf3099e57d08af716eee21eda45
gretaangebrand <ag9716@mci4me.at>
2024-06-04 12:09:36 +0200

    Versuch Plot HR average

--- ekgdata.py
+++ ekgdata.py
@@ -9,8 +9,6 @@ import scipy.signal as signal
 
 # Methode für find_peaks erstellen
 
-
-
 class EKGdata:
 
 ## Konstruktor der Klasse soll die Daten einlesen
@@ -44,6 +42,10 @@ class EKGdata:
         #thresold auf größeren Wert setzen, um nur Peaks zu finden, die höher als 360 mV sind
         self.peaks = signal.find_peaks(x, height=340)
         return self.peaks
+    
+    #neuen DataFrame erstellen, der auf den Zeitpunkten der Peaks basiert
+    def get_peaks_df(self):
+        return self.df.iloc[self.peaks[0]]
 
     def make_plot(self):
 
@@ -53,6 +55,18 @@ class EKGdata:
         # show the peaks in the plot that are above the threshold
         self.fig.add_scatter(x=self.df["Zeit in ms"].iloc[self.peaks[0]], y=self.df["Messwerte in mV"].iloc[self.peaks[0]], mode='markers', marker=dict(color='blue', size=8))
         #return self.fig 
+
+    # errechne die Heart Rate aus dem neuen DataFrame mit den Peaks
+    def estimate_hr_peaks(self):
+        # Zeitdifferenz zwischen den Peaks in ms
+        rr_intervall = self.get_peaks_df()["Zeit in ms"].diff()
+        # Heart Rate steht im Verhältnis zur Zeitdifferenz mit dem Kehrwert HeartRate = 1/RR-Intervall
+        self.hr = 1 / rr_intervall
+        return self.hr
+    
+    def make_plot_hr(self):
+        self.fig_hr = px.line(x=self.get_peaks_df()["Zeit in ms"], y=self.hr)
+        return self.fig_hr
 
 
 if __name__ == "__main__":
@@ -68,3 +82,15 @@ if __name__ == "__main__":
     #print(ekg_dict)
     #ekg = EKGdata(ekg_dict)
     #print(ekg.df.head())
+    
+    #Zeige Graf
+    ekg.find_peaks()
+    ekg.make_plot()
+    ekg.fig.show()
+
+    #Zeige Plot der Peaks
+    ekg.get_peaks_df()
+    ekg.estimate_hr_peaks()
+    ekg.make_plot_hr()
+    ekg.fig_hr.show()
+
