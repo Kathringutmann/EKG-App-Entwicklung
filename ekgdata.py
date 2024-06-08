f526624b8930671f3777bdb43c3a8e5533badfbc
Kathringutmann <gk2575@mci4me.at>
2024-06-08 15:20:28 +0200

    Load_by_id +Slider ("plot_time_series()") FERTIG

--- ekgdata.py
+++ ekgdata.py
@@ -3,85 +3,43 @@ import pandas as pd
 import plotly.express as px
 import scipy.signal as signal
 
-# %% Objekt-Welt
-
-# Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
-
-# Methode für find_peaks erstellen
-
-
-
 class EKGdata:
-
-## Konstruktor der Klasse soll die Daten einlesen
-
     @staticmethod
-    def load_by_id(person_data,id):
-        """ Eine Funktion"""
-        
+    def load_by_id(person_data, id):
         for person in person_data:
             for test in person["ekg_tests"]:
                 if test["id"] == id:
-                    
                     return test
         return {}
-        
 
     def __init__(self, ekg_dict):
-        #pass
         self.id = ekg_dict["id"]
         self.date = ekg_dict["date"]
         self.data = ekg_dict["result_link"]
         self.df = pd.read_csv(self.data, sep='\t', header=None, names=['Messwerte in mV','Zeit in ms'])
-        
-        self.df['Zeit in ms'] = self.df['Zeit in ms']-self.df['Zeit in ms'].min()
-        #self.df = self.df.head(10000)
-
+        self.df['Zeit in ms'] = self.df['Zeit in ms'] - self.df['Zeit in ms'].min()
 
     def find_peaks(self):
         x = self.df["Messwerte in mV"]
-        #thresold auf größeren Wert setzen, um nur Peaks zu finden, die höher als 360 mV sind
         self.peaks = signal.find_peaks(x, height=340)
         return self.peaks
 
     def make_plot(self):
-        print(self.df.tail())
-        # Erstellte einen Line Plot, mit der Zeit aus der x-Achse
         self.fig = px.line(self.df, x="Zeit in ms", y="Messwerte in mV")
+        self.fig.add_scatter(x=self.df["Zeit in ms"].iloc[self.peaks[0]], y=self.df["Messwerte in mV"].iloc[self.peaks[0]], mode='markers', marker=dict(color='red', size=8))
 
-        # show the peaks in the plot that are above the threshold
-        self.fig.add_scatter(x=self.df["Zeit in ms"].iloc[self.peaks[0]], y=self.df["Messwerte in mV"].iloc[self.peaks[0]], mode='markers', marker=dict(color='blue', size=8))
-       # Erstellte einen Line Plot, mit der Zeit aus der x-Achse
-        #self.fig = px.line(self.df, x="Zeit in ms", y="Messwerte in mV")
-
-    def update_axis(self,x_min,y_max):        
-        # Set the x-axis range to 2.5 seconds
-            self.fig.update_layout(
-        xaxis=dict(range=[x_min, y_max])
-    )
-
-    
-    # show the peaks in the plot that are above the threshold
-        #self.fig.add_scatter(x=self.df["Zeit in ms"].iloc[self.peaks[0]], y=self.df["Messwerte in mV"].iloc[self.peaks[0]], mode='markers', marker=dict(color='blue', size=8))
-
+    def update_axis(self, x_min, x_max):
+        self.fig.update_layout(
+            xaxis=dict(range=[x_min, x_max])
+        )
 
 if __name__ == "__main__":
     file = open("data/person_db.json")
     person_data = json.load(file)
-    test_dict = EKGdata.load_by_id(person_data,3)
+    test_dict = EKGdata.load_by_id(person_data, 3)
     ekg = EKGdata(test_dict)
-    
-    #print(ekg.df["Zeit in ms"].value_counts())
-    #print(ekg.id)
-    
 
-      # Find the peaks
     ekg.find_peaks()
-    
-    # Create the plot
     ekg.make_plot()
-    ekg.update_axis(0,5000)
-    # Display the plot
+    ekg.update_axis(0, 5000)
     ekg.fig.show()
-    
-    
--- main.py
+++ main.py
@@ -2,57 +2,54 @@ import streamlit as st
 from read_data import get_name, get_person_data, find_person_data_by_name
 import person
 import ekgdata
-
-#person_data = get_person_data()
-#names = get_name(person_data)
+from PIL import Image
 
 person_data = person.Person.load_person_data()
 names = person.Person.get_person_list(person_data)
-    
-current_person = None  
-    
-#Überschrift
-st.title('EKG Analyse')
+current_person = None
 
-from PIL import Image
+st.title('EKG Analyse')
 
 col1, col2 = st.columns(2)
 
 with col1:
-   st.write("## Versuchsperson auswählen") #Unterüberschrift
-   current_user = st.selectbox(
-    'Versuchsperson',
-    options = names, key="sbVersuchspersonen") #Auswahlbox
-   
-   current_person = person.Person(find_person_data_by_name(current_user))
-   test_ids = person.Person.get_test_ids(current_person)
-     
-   
-   image = Image.open(current_person.picture_path)
-   
-
-   st.write("currently selected user is: "+ current_user)
-   st.write("age of current user: " + str(current_person.get_age()))
-   st.write ("max heartrate calculated by age is: "+ str(current_person.calc_max_hr()))
+    st.write("## Versuchsperson auswählen")
+    current_user = st.selectbox('Versuchsperson', options=names, key="sbVersuchspersonen")
+    current_person = person.Person(find_person_data_by_name(current_user))
+    test_ids = person.Person.get_test_ids(current_person)
+    image = Image.open(current_person.picture_path)
+    st.write("currently selected user is: " + current_user)
+    st.write("age of current user: " + str(current_person.get_age()))
+    st.write("max heartrate calculated by age is: " + str(current_person.calc_max_hr()))
 
 with col2:
-   st.header("Bild")
-   st.image(image, caption=current_user)
-   
-   selected_test_id = st.selectbox('Wählen Sie eine Test-ID:', test_ids)
+    st.header("Bild")
+    st.image(image, caption=current_user)
+    selected_test_id = st.selectbox('Wählen Sie eine Test-ID:', test_ids)
+    st.write(f'Sie haben die Test-ID {selected_test_id} ausgewählt.')
 
-   st.write(f'Sie haben die Test-ID {selected_test_id} ausgewählt.')
+    test_dict = ekgdata.EKGdata.load_by_id(person_data, selected_test_id)
+    ekg = ekgdata.EKGdata(test_dict)
+    ekg.find_peaks()
+    ekg.make_plot()
 
+    # Gesamtgröße des EKG-Datensatzes in ms (angenommen 600000 ms)
+    total_size_ms = 600000
 
-   test_dict = ekgdata.EKGdata.load_by_id(person_data,selected_test_id)
-   ekg = ekgdata.EKGdata(test_dict)
-   
-   ekg.find_peaks()
-   ekg.make_plot()
-   y_min = st.slider("EKG Frame Range", 0, 200000, 1000)#Slider löschen???
-   y_min = st.slider("Untere Grenze", 0, 100, 0)
-   y_max = st.slider("Obere Grenze", 0, 100, 100)#Slider anpassen... zusammen fügen, damit die zwei verbunden sind und parallel verschiebbar sind
-   st.write(f"Der ausgewählte Wert ist: {y_min}")
-   ekg.update_axis(y_min,5000)
-   
-   st.plotly_chart(ekg.fig)
+    # Slider zur Einstellung der Größe des ausgewählten Bereichs in ms
+    range_size = st.slider("Ausschnittsgröße (ms)", 1000, total_size_ms, 5000)
+
+    # Slider zur Verschiebung des ausgewählten Bereichs entlang des EKG-Grafiks in ms
+    range_start = st.slider("Verschiebung (ms)", 0, total_size_ms - range_size, 0)
+
+    # Bereichsende berechnen
+    range_end = range_start + range_size
+
+    # Anzeige der gewählten Bereiche
+    st.write(f"Gewählter Bereich: {range_start} ms bis {range_end} ms")
+
+    # Update plot with selected range
+    ekg.update_axis(range_start, range_end)
+
+    # Plot the updated figure
+    st.plotly_chart(ekg.fig)
