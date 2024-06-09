447637ef11bff7604a70e92972c48b4077c5f0b7
gretaangebrand <ag9716@mci4me.at>
2024-06-09 18:07:21 +0200

    finale Version ekgdata.py und main.py

--- ekgdata.py
+++ ekgdata.py
@@ -1,25 +1,16 @@
 import json
 import pandas as pd
-import plotly.express as px
-import scipy.signal as signal
-import streamlit as st
-import plotly.graph_objects as go
-
+import plotly.express as px #Bibliothek für interaktive Plots
+import scipy.signal as signal #Bibliothek für Signalverarbeitung
 
 # %% Objekt-Welt
-
-# Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
-
-# Methode für find_peaks erstellen
 
 class EKGdata:
 
 ## Konstruktor der Klasse soll die Daten einlesen
-
     @staticmethod
     def load_by_id(person_data,id):
         """ Eine Funktion"""
-        
         for person in person_data:
             #print(person)
             for test in person["ekg_tests"]:
@@ -27,27 +18,19 @@ class EKGdata:
                 if test["id"] == id:
                     #print(id)
                     return test
-
         return {}
-
-
+        
     def __init__(self, ekg_dict):
-        #übergibt die Daten aus dem Dictionary in die Klasse
+        #Konstruktor der Klasse, der die Daten einliest
         self.id = ekg_dict["id"]
         self.date = ekg_dict["date"]
         self.data = ekg_dict["result_link"]
         self.df = pd.read_csv(self.data, sep='\t', header=None, names=['Messwerte in mV','Zeit in ms',])
-        self.peaks = self.find_peaks()
-        self.peaks_plot = self.get_peaks_df()  
-        self.hr = self.estimate_hr_peaks()
-        self.fig = None
-        self.fig_hr = None
-        self.heartrate_time = pd.DataFrame({"Time in s": self.peaks_plot["Zeit in ms"]/1000, "Heartrate": self.hr})
-        
 
+    #Methode für Peakfinder erstellen
     def find_peaks(self):
         x = self.df["Messwerte in mV"]
-        #thresold auf größeren Wert setzen, um nur Peaks zu finden, die höher als 340 mV sind
+        #thresold auf größeren Wert setzen, um nur Peaks zu finden, die höher als 360 mV sind
         self.peaks = signal.find_peaks(x, height=340)
         return self.peaks
     
@@ -55,33 +38,42 @@ class EKGdata:
     def get_peaks_df(self):
         return self.df.iloc[self.peaks[0]]
 
-    def make_plot(self):
-
+    #Methode für Plot erstellen
+    def plot_time_series(self):
         # Erstellte einen Line Plot, mit der Zeit aus der x-Achse
-        self.fig = px.line(self.df, x="Zeit in ms", y="Messwerte in mV")
-        self.fig_hr = px.line(self.heartrate_time, x="Time in s", y="Heartrate", title="Herzfrequenz über die Zeit")
-
+        self.fig = px.line(self.df, x="Zeit in ms", y="Messwerte in mV",
+                           range_x=[100,110],
+                           range_y=[self.df["Messwerte in mV"].min(), self.df["Messwerte in mV"].max()])
         # show the peaks in the plot that are above the threshold
-        self.fig.add_scatter(x=self.df["Zeit in ms"].iloc[self.peaks[0]], y=self.df["Messwerte in mV"].iloc[self.peaks[0]], mode='markers', marker=dict(color='blue', size=8))
-        #return self.fig 
+        self.fig.add_scatter(x=self.df["Zeit in ms"].iloc[self.peaks[0]], y=self.df["Messwerte in mV"].iloc[self.peaks[0]], mode='markers' ,marker=dict(color='red', size=8), name='Peaks')
+        #achsenskalierung anpassen
+        self.fig.update_yaxes(range=[self.df["Messwerte in mV"].min(), self.df["Messwerte in mV"].max()])
+        self.fig.update_xaxes(range=[self.df["Zeit in ms"].min(), self.df["Zeit in ms"].max()])
 
+        return self.fig 
+
+
+    #Methode für die Herzfrequenz erstellen
     # errechne die Heart Rate aus dem neuen DataFrame mit den Peaks
     def estimate_hr_peaks(self):
         # Zeitdifferenz zwischen den Peaks in ms
         rr_intervall = self.get_peaks_df()["Zeit in ms"].diff()
         # Heart Rate steht im Verhältnis zur Zeitdifferenz mit dem Kehrwert HeartRate = 1/RR-Intervall
         self.hr = 1 / rr_intervall
+        #überschrift für die Spalte erstellen
         return self.hr
     
-    # Binde den Plot in Streamlit ein
-    def show_plot(self):
-        st.plotly_chart(self.fig)
-        st.plotly_chart(self.fig_hr)
+    #Methode für den Plot der Herzfrequenz erstellen
+    def make_plot_hr(self):
+        peaks_df = self.get_peaks_df().iloc[1:] #ersten Wert entfernen, da er nicht relevant ist
+        self.fig_hr = px.line(x=self.get_peaks_df()["Zeit in ms"], y=self.hr, title='Heart Rate Over Time', labels={'x': 'Time (ms)', 'y': 'Heart Rate (bpm)'})
+        #return self.fig_hr
 
+# Testen der Klasse
 if __name__ == "__main__":
     file = open("data/person_db.json")
     person_data = json.load(file)
-    test_dict = EKGdata.load_by_id(person_data,2)
+    test_dict = EKGdata.load_by_id(person_data,2) 
     ekg = EKGdata(test_dict)
     print(ekg.id)
     print(ekg.date)
@@ -90,20 +82,21 @@ if __name__ == "__main__":
     print(ekg.peaks_plot)
     print(ekg.hr)
     print(ekg.heartrate_time)
-
+    
+    #print("This is a module with some functions to read the EKG data")
+    
     ekg_dict = person_data[0]["ekg_tests"][0]
     print(ekg_dict)
     ekg = EKGdata(ekg_dict)
     print(ekg.df.head())
     
-    #Zeige Graf der Peaks
+    #Zeige Graf
     ekg.find_peaks()
-    ekg.make_plot()
+    ekg.plot_time_series()
     ekg.fig.show()
 
-    #Zeige Plot zwischen Herzfrequenz und Zeit
+    #Zeige Plot der Peaks
     ekg.get_peaks_df()
     ekg.estimate_hr_peaks()
-    ekg.make_plot()
+    ekg.make_plot_hr()
     ekg.fig_hr.show()
-
--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import streamlit as st
 from read_data import get_name, get_person_data, find_person_data_by_name
 import person
+from ekgdata import EKGdata
+from PIL import Image #Bibliothek für Bilder
 
-#person_data = get_person_data()
-#names = get_name(person_data)
 
-person_data = person.Person.load_person_data()
-names = person.Person.get_person_list(person_data)
+person_data = person.Person.load_person_data() #Personendaten laden
+names = person.Person.get_person_list(person_data) #Namen der Personen in Liste speichern
     
-current_person = None  
+current_person = None  #Variable für aktuelle Person
     
 #Überschrift
 st.title('EKG Analyse')
 
-from PIL import Image
-
-col1, col2 = st.columns(2)
+col1, col2 = st.columns(2) #Spalten erstellen
 
 with col1:
    st.write("## Versuchsperson auswählen") #Unterüberschrift
@@ -23,16 +21,31 @@ with col1:
     'Versuchsperson',
     options = names, key="sbVersuchspersonen") #Auswahlbox
    
+   #Personenobjekt erstellen
    current_person = person.Person(find_person_data_by_name(current_user))
 
      
-   
+   #Bild der Person anzeigen
    image = Image.open(current_person.picture_path)
    
-
+  
    st.write("currently selected user is: "+ current_user)
    st.write("age of current user: " + str(current_person.get_age()))
    st.write ("max heartrate calculated by age is: "+ str(current_person.calc_max_hr()))
+
+   if current_person:
+      test_dict = EKGdata.load_by_id(person_data, current_person.id)
+      ekg = EKGdata(test_dict)
+      ekg.find_peaks()
+      ekg.plot_time_series()
+      st.plotly_chart(ekg.fig)
+      hr = ekg.estimate_hr_peaks()
+
+      #st.write(f"Estimated Heart Rate: {hr.mean():.2f} bpm")
+      
+      ekg.make_plot_hr()
+      st.plotly_chart(ekg.fig_hr)
+      
 
 with col2:
    st.header("Bild")
